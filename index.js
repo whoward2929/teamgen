b5d843932d6e840e287d4160c61c60d6b9c360cf
whoward2929 <Whoward939@gmail.com>
2021-08-29 22:35:53 -0400

    concerns

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const inquirer = require("inquirer")
+const Manager = require('.lib/Manager');  
+const Engineer = require('lib/Engineer');
+const Employee = require('lib/Employee');
+const Intern = require('lib/Intern');
+const inquirer = require('inquirer');
+const fs = require ('fs');
+const path = require('path');
 
-const askManager = function(){
-    
-}
 
-askManager()
+const questoins [
+    {name: 'name', message:"whats the employees name" },
+    {name: 'id', message: "whats the employees id"},
+    {name: 'emial', message: "whats the employees email"}
+]
